55138ff7616ae5fd00c2983cb4a6f28484229cd9
Aron Vaupel <aronvaupel@Arons-MacBook-Pro.local>
2022-03-30 07:45:05 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import * as React from "react";
+import { ChakraProvider, Box, Grid, theme } from "@chakra-ui/react";
+import Contact from "./components/ContactForm";
+
+export const App = () => (
+  <ChakraProvider theme={theme}>
+    <Box textAlign="center" fontSize="xl">
+      <Grid minH="100vh" p={3}>
+        <Contact />
+      </Grid>
+    </Box>
+  </ChakraProvider>
+);
